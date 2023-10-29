c4d34326e1d4a3d6058ea5e6e029e61266a0954a
PierreSQS <nomo_messina@web.de>
2023-10-29 22:30:48 +0100

    Sec2_ChapXXX: added invalid Body LoansController test

--- section2-sb315/loans/src/test/java/com/eazybytes/loans/controller/LoansControllerTest.java
+++ section2-sb315/loans/src/test/java/com/eazybytes/loans/controller/LoansControllerTest.java
@@ -2,8 +2,10 @@ package com.eazybytes.loans.controller;
 
 import com.eazybytes.loans.constants.LoansConstants;
 import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.exception.ResourceNotFoundException;
 import com.eazybytes.loans.service.ILoansService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashSet;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class LoansControllerTest {
 
     public static final String API_CREATE_URL = "/api/create";
+    public static final String UPDATE_API_URL = "/api/update";
     @Autowired
     ObjectMapper objectMapper;
 
@@ -73,11 +82,69 @@ class LoansControllerTest {
     }
 
     @Test
-    void fetchLoanDetails() {
+    void fetchLoanDetails_MobileNr_OK()  throws Exception {
+        // Given
+        given(iLoansServMock.fetchLoan(anyString())).willReturn(loansDto);
+
+        mockMvc.perform(get("/api/fetch")
+                        .param("mobileNumber",loansDto.getMobileNumber()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.loanNumber")
+                        .value(loansDto.getLoanNumber()))
+                .andDo(print());
     }
 
     @Test
-    void updateLoanDetails() {
+    void fetchLoanDetails_MobileNr_Invalid()  throws Exception {
+        // Given
+        ResourceNotFoundException rnfe = new ResourceNotFoundException("Loan", "mobileNumber",
+                loansDto.getMobileNumber());
+        doThrow(rnfe).when(iLoansServMock).fetchLoan(loansDto.getMobileNumber());
+
+        mockMvc.perform(get("/api/fetch")
+                        .param("mobileNumber",loansDto.getMobileNumber()))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errorMessage")
+                        .value(containsString("Loan not found")))
+                .andDo(print());
+    }
+
+    @Test
+    void updateLoanDetails_Updated() throws Exception {
+        // Given
+        given(iLoansServMock.updateLoan(any(LoansDto.class))).willReturn(Boolean.TRUE);
+        mockMvc.perform(put(UPDATE_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loansDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusMsg").value(LoansConstants.MESSAGE_200))
+                .andDo(print());
+    }
+
+    @Test
+    void updateLoanDetails_InvalidBody() throws Exception {
+        // Given
+        ConstraintViolationException cvex =
+                new ConstraintViolationException("updateLoanDetails.mobileNumber",new HashSet<>());
+        doThrow(cvex).when(iLoansServMock).updateLoan(loansDto);
+
+        mockMvc.perform(put(UPDATE_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loansDto)))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    void updateLoanDetails_NotFound() throws Exception {
+        // Given
+        given(iLoansServMock.updateLoan(any(LoansDto.class))).willReturn(Boolean.FALSE);
+        mockMvc.perform(put(UPDATE_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loansDto)))
+                .andExpect(status().isExpectationFailed())
+                .andExpect(jsonPath("$.statusMsg").value(LoansConstants.MESSAGE_417_UPDATE))
+                .andDo(print());
     }
 
     @Test
