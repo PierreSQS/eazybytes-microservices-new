2ce93a4b355f3fb50f90672dcfc4c7cf86653792
PierreSQS <nomo_messina@web.de>
2023-12-05 14:03:02 +0100

    Sec9_ChapXXX: initial commit

---
+++ section9-sb316/accounts/src/test/java/com/eazybytes/accounts/controller/CustomerControllerTest.java
@@ -0,0 +1,85 @@
+package com.eazybytes.accounts.controller;
+
+import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.AccountsDto;
+import com.eazybytes.accounts.dto.CardsDto;
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.LoansDto;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.service.ICustomersService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ICustomersService customersServMock;
+
+    CustomerDetailsDto customerDetailsDto;
+
+    Customer customer;
+
+    AccountsDto accountsDto;
+    CardsDto cardsDto;
+
+    LoansDto loansDto;
+
+    @BeforeEach
+    void setUp() {
+        accountsDto =  new AccountsDto();
+        accountsDto.setAccountType(AccountsConstants.SAVINGS);
+        accountsDto.setAccountNumber(1234567890L);
+
+        cardsDto = new CardsDto();
+        cardsDto.setCardNumber("1234567890");
+        cardsDto.setCardType("Credit Card");
+        cardsDto.setMobileNumber("1111111111");
+
+        loansDto = new LoansDto();
+        loansDto.setLoanNumber("2222222222");
+        loansDto.setLoanType("Home Loan");
+
+        customer = new Customer();
+        customer.setCustomerId(12121212121L);
+        customer.setMobileNumber("1111111111");
+        customer.setName("Test User");
+        customer.setEmail("test@example.com");
+
+        customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(accountsDto);
+        customerDetailsDto.setCardsDto(cardsDto);
+        customerDetailsDto.setLoansDto(loansDto);
+    }
+
+    @Test
+    void fetchCustomerDetails_MobileNumber_OK() throws Exception {
+        // Given
+        given(customersServMock.fetchCustomerDetails(customerDetailsDto.getMobileNumber())).willReturn(customerDetailsDto);
+        mockMvc.perform(get("/api/fetchCustomerDetails")
+                        .param("mobileNumber", customerDetailsDto.getMobileNumber()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value(customerDetailsDto.getName()))
+                .andExpect(jsonPath("accountsDto.accountNumber")
+                        .value(customerDetailsDto.getAccountsDto().getAccountNumber()))
+                .andExpect(jsonPath("loansDto.loanNumber")
+                        .value(customerDetailsDto.getLoansDto().getLoanNumber()))
+                .andDo(print());
+    }
+}
