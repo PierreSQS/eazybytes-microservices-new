0872232371a4e09df789f50cb6b07a80c5ae8920
PierreSQS <nomo_messina@web.de>
2023-12-06 09:18:04 +0100

    Sec9_Chap124-Implementing Custom Routing using Spring Cloud Gateway-NOK: implemented new routes

--- section9-sb316/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ section9-sb316/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,36 @@ package com.eazybytes.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
+	private static final String SEGMENT = "/${segment}";
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p.path("/eazybank/accounts/**")
+						.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)",
+								SEGMENT))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p.path("/eazybank/loans/**")
+						.filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)",
+								SEGMENT))
+						.uri("lb://LOANS"))
+				.route(p -> p.path("/eazybank/cards/**")
+						.filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)",
+								SEGMENT))
+						.uri("lb://CARDS"))
+				.build();
 	}
 
 }
