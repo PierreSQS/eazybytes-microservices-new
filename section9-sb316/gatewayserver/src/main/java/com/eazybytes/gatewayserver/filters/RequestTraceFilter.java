25b28d7836230708595ae52577e40b52721e391a
PierreSQS <nomo_messina@web.de>
2023-12-08 01:04:52 +0100

    Sec9_Chap126-Cross cutting concerns Tracing & Logging - Part 1: Pierrot Optimizations

--- section9-sb316/gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/RequestTraceFilter.java
+++ section9-sb316/gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/RequestTraceFilter.java
@@ -1,8 +1,7 @@
 package com.eazybytes.gatewayserver.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
@@ -13,33 +12,28 @@ import reactor.core.publisher.Mono;
 
 @Order(1)
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class RequestTraceFilter implements GlobalFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(RequestTraceFilter.class);
-
-    @Autowired
-    FilterUtility filterUtility;
+    private final FilterUtility filterUtility;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
         if (isCorrelationIdPresent(requestHeaders)) {
-            logger.debug("eazyBank-correlation-id found in RequestTraceFilter : {}",
+            log.debug("eazyBank-correlation-id found in RequestTraceFilter : {}",
                     filterUtility.getCorrelationId(requestHeaders));
         } else {
             String correlationID = generateCorrelationId();
             exchange = filterUtility.setCorrelationId(exchange, correlationID);
-            logger.debug("eazyBank-correlation-id generated in RequestTraceFilter : {}", correlationID);
+            log.debug("eazyBank-correlation-id generated in RequestTraceFilter : {}", correlationID);
         }
         return chain.filter(exchange);
     }
 
     private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
-        if (filterUtility.getCorrelationId(requestHeaders) != null) {
-            return true;
-        } else {
-            return false;
-        }
+        return filterUtility.getCorrelationId(requestHeaders) != null;
     }
 
     private String generateCorrelationId() {
--- section9-sb316/gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/ResponseTraceFilter.java
+++ section9-sb316/gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/ResponseTraceFilter.java
@@ -1,31 +1,29 @@
 package com.eazybytes.gatewayserver.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
+/**
+ * Custom Filter for the Response Header
+ */
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class ResponseTraceFilter {
-
-    private static final Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
-
-    @Autowired
-    FilterUtility filterUtility;
+    private final FilterUtility filterUtility;
 
     @Bean
     public GlobalFilter postGlobalFilter() {
-        return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
-            }));
-        };
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+            String correlationId = filterUtility.getCorrelationId(requestHeaders);
+            log.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+            exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+        }));
     }
 }
