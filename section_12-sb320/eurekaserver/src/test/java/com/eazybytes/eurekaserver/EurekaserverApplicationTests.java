95a159ed4624ca5b2b619648d9fdccad18d6f719
PierreSQS <nomo_messina@web.de>
2023-12-21 12:40:51 +0100

    Sec12_ChapXXX: initial commit (Sec11-SB320 Code)

---
+++ section_12-sb320/eurekaserver/src/test/java/com/eazybytes/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
+	}
+
+}
