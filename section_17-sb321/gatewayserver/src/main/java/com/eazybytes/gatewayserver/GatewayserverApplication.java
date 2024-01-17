93071f58ebd0ef765d9e62dab1491c924b599cd8
PierreSQS <nomo_messina@web.de>
2024-01-17 21:49:37 +0100

    Sec17_Chap253-Making K8s Discovery Client changes in µServ.- Part2: code related changes in Gatewayserver

--- section_17-sb321/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ section_17-sb321/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -19,6 +20,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableDiscoveryClient // K8S DISCOVERY CLIENT
 public class GatewayserverApplication {
 
 	public static final String SEGMENT = "/${segment}";
@@ -37,7 +39,7 @@ public class GatewayserverApplication {
 										.addResponseHeader(X_RESPONSE_TIME, LocalDateTime.now().toString())
 										.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
 												.setFallbackUri("forward:/contactSupport")))
-								.uri("lb://ACCOUNTS"))
+								.uri("http://accounts-service:8080"))
 					.route(p -> p
 							.path("/eazybank/loans/**")
 							.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", SEGMENT)
@@ -45,14 +47,14 @@ public class GatewayserverApplication {
 									.retry(retryConfig -> retryConfig.setRetries(3)
 											.setMethods(HttpMethod.GET)
 											.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
-							.uri("lb://LOANS"))
+							.uri("http://loans-service:8090"))
 					.route(p -> p
 							.path("/eazybank/cards/**")
 							.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", SEGMENT)
 									.addResponseHeader(X_RESPONSE_TIME, LocalDateTime.now().toString())
 									.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
 											.setKeyResolver(userKeyResolver())))
-							.uri("lb://CARDS")).build();
+							.uri("http://cards-service:9000")).build();
 	}
 
 	@Bean
